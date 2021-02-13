68640aa4bb4c59e5fb0351bb6492b750243431b6
Sohel Shaikh <49446910+Sohelshaikh7@users.noreply.github.com>
2021-02-13 22:42:30 +0530

    Day 10
    With some minor changes and corrections

---
+++ Python Mini Projects/Snake_Water_Gun.py
@@ -0,0 +1,53 @@
+import random
+
+# Snake Water Gun / Rock Paper Scissors
+
+
+def gameWin(comp, you):
+    # if two values are equal, declare a TIE
+    if comp == you:
+        return None
+
+    # Check for all possibilities when computer chose s
+    elif comp == 's':
+        if you == 'w':
+            return False
+        elif you == 'g':
+            return True
+
+    # Check for all possibilities when computer chose w
+    elif comp == 'w':
+        if you == 'g':
+            return False
+        elif you == 's':
+            return True
+
+    # Check for all possibilities when computer chose g
+    elif comp == 'g':
+        if you == 's':
+            return False
+        elif you == 'w':
+            return True
+
+
+print("Computer Turn: Snake(s) Water(w) or Gun()g?")
+randNo = random.randint(1, 3)
+if randNo == 1:
+    comp = 's'
+elif randNo == 2:
+    comp = 'w'
+elif randNo == 3:
+    comp = 'g'
+
+you = input("Your Turn: Snake(s) Water(w) or Gun()g?\n")
+a = gameWin(comp, you)
+
+print(f"Computer chose {comp}")
+print(f"You chose {you}")
+
+if a == None:
+    print("The game is a TIE!!")
+elif a:
+    print("You WIN!!")
+else:
+    print("You LOSE!!")
